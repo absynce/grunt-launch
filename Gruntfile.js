e0a8d2ca17894dc7265fad895bc07cd8d37e9c47
Jared M. Smith <absynce@gmail.com>
2013-06-24 04:55:07 +0000

    Add grunt-release configuration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,8 +43,7 @@ module.exports = function(grunt) {
             installDependencies: true,
             createVersionedDir: true,
             moveTempToVersioned: true,
-            symbolicLink: true,
-            restart: true,
+            symbolicLink: true
         },
 
         // Unit tests.
@@ -52,6 +51,14 @@ module.exports = function(grunt) {
             tests: ['test/*_test.js'],
         },
 
+        // Release
+        release: {
+            options: {
+                npm: false,
+                commitMessage: 'Release <%= version %>',
+                tagMessage: 'Release <%= version %>'
+            }
+        }
     });
 
     // Actually load this plugin's task(s).
@@ -61,6 +68,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-release');
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
