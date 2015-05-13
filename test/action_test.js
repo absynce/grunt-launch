95e15d9cbd85027d8ec347607d759a5d02e1030c
Jared M. Smith <absynce@gmail.com>
2015-05-13 15:28:16 +0000

    Adding test for space command issue #1. Still broken.

---
+++ test/action_test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var grunt = require('grunt');
+var action = require('../index')(grunt).action;
+
+exports.action = {
+  local: {
+    setUp: function (done) {
+      done();
+    },
+    testSpaceCommand: function (test) {
+      // test space command here
+      action.local('grep -e "function (" ../Gruntfile.js', function (exitcode) {
+        if (exitcode === 0) {
+          action.success('Command with spaces successfully run!');
+        }
+        else {
+          action.error('Command with spaces failed :(');
+          test.ok(false, 'Command failed with exit code: ' + exitcode);
+        }
+        test.done();
+      });
+    }
+  }
+};
